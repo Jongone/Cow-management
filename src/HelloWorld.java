302c7c37e217b265223ee46f3a6abb7116ff5966
이종원 <@DESKTOP-NLANV17>
2020-03-21 10:55:39 +0900

    Create a menu

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("-Select Menu-");
+		System.out.println("1.Add Cow");
+		System.out.println("2.Delete Cow");
+		System.out.println("3.Edit Cow");
+		System.out.println("4.View Cow");
+		System.out.println("Exit");
+	}
+}
