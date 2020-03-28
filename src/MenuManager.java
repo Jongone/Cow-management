e0f4369df22a2c5ee7cd6ee658d39926f0591edb
이종원 <@DESKTOP-NLANV17>
2020-03-29 01:04:02 +0900

    add menu selection

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,11 +1,69 @@
-public class HelloWorld {
+import java.util.Scanner;
+
+public class MenuManager {
 
 	public static void main(String[] args) {
-		System.out.println("-Select Menu-");
-		System.out.println("1.Add Cow");
-		System.out.println("2.Delete Cow");
-		System.out.println("3.Edit Cow");
-		System.out.println("4.View Cow");
-		System.out.println("Exit");
+		Scanner sc = new Scanner(System.in);
+		int num = 6;
+
+		while(num!=7) {
+			System.out.println("-Select Menu-");
+			System.out.println("1.Add Cow");
+			System.out.println("2.Delete Cow");
+			System.out.println("3.Edit Cow");
+			System.out.println("4.View Cow");
+			System.out.println("5.Sold List");
+			System.out.println("6.Show Menu");
+			System.out.println("7.Exit");
+			num = sc.nextInt();
+			if (num == 1) {
+				addCow();
+			}
+			else if (num == 2) {
+				deleteCow();
+			}
+			else if (num == 3) {
+				editCow();
+			}
+			else if (num == 4) {
+				viewCow();
+			}
+			else if (num == 5) {
+				soldList();
+			}
+			else {
+				continue;
+			}
+		}
+	}
+	public static void addCow() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Cow ID:");
+		int cowid = sc.nextInt();
+		System.out.println("Cow Sex:");
+		String cowsex = sc.next();
+		System.out.println("Cow weight:");
+		double cowweight = sc.nextDouble();
+	}
+	public static void deleteCow() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Cow ID:");
+		int cowid = sc.nextInt();
+	}
+	public static void editCow() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Cow ID:");
+		int cowid = sc.nextInt();
+	}
+	public static void viewCow() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Cow ID:");
+		int cowid = sc.nextInt();
+	}
+	public static void soldList() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Cow ID:");
+		int cowid = sc.nextInt();
 	}
 }
+	
